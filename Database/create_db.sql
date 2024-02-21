060de896fe29651afdd1447d8060c74c54da8ed3
Skander <lahbaeil@gmail.com>
2024-02-22 00:35:26 +0100

    Added the NGINX reverse proxy

--- Database/create_db.sql
+++ Database/create_db.sql
@@ -1,11 +1,17 @@
 CREATE DATABASE recipes_db;
 
+\c recipes_db
+
 CREATE TABLE recipes (
     title text,
     ingredients text, 
     instructions text
 );
 
-CREATE USER recipes_user WITH LOGIN;
+CREATE USER recipe_user WITH LOGIN;
 
-ALTER ROLE recipes_user WITH PASSWORD 'user'; 
+ALTER ROLE recipe_user WITH PASSWORD 'user'; 
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO recipe_user;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO recipe_user;
+GRANT ALL PRIVILEGES ON DATABASE recipes_db TO recipe_user;
